67bad392b84dfec743c378b2084a801470ff5d13
divyansh7271 <divyansh7271@gmail.com>
2022-08-04 11:02:00 +0530

    added 0/1 knapsack

--- bucket.py
+++ bucket.py
@@ -13,14 +13,16 @@ def insertion_sort(bucket):
               
 def bucket_sort(input_list):
     max_value = max(input_list)
+    print(max_value,end=" ")
     size = max_value/len(input_list)
-
+    print(size)
     buckets_list= []
     for x in range(len(input_list)):
         buckets_list.append([]) 
 
     for i in range(len(input_list)):
         j = int (input_list[i] / size)
+        print(f"j -- >{j}")
         if j != len (input_list):
             buckets_list[j].append(input_list[i])
         else:
@@ -28,35 +30,45 @@ def bucket_sort(input_list):
 
     for z in range(len(input_list)):
         insertion_sort(buckets_list[z])
-        
-    final_output = []
+
+    final_output = []    
     for x in range(len (input_list)):
         final_output = final_output + buckets_list[x]
     return final_output
 
-if __name__ == '__main__': 
-    exe_mer = []
-    c = []    
-    for i in range(2, 1300, 90):
-        array = []
-        for j in range(0, i):
-            temp = random.uniform(0,1)
-            array.append(temp)
+# if __name__ == '__main__': 
+#     exe_mer = []
+#     c = []    
+#     for i in range(2, 1300, 90):
+#         array = []
+#         for j in range(0, i):
+#             temp = random.uniform(0,1)
+#             array.append(temp)
             
-        start = time.perf_counter_ns()
-        bucket_sort(array)
-        end = time.perf_counter_ns()
+#         start = time.perf_counter_ns()
+#         bucket_sort(array)
+#         end = time.perf_counter_ns()
         
-        c.append(i)
-        exe_mer.append((end - start))
+#         c.append(i)
+#         exe_mer.append((end - start))
      
     
-    l = [x*3000 for x in c]
-    plt.plot(c,l)
-    plt.plot(c,exe_mer)
-    plt.xlabel('No of inputs')
-    plt.ylabel('Time taken (in ns)')
-    plt.legend(["n","Bucket-sort"])
-    plt.title("Bucket_sort")
-    plt.show()
+#     l = [x*3000 for x in c]
+#     plt.plot(c,l)
+#     plt.plot(c,exe_mer)
+#     plt.xlabel('No of inputs')
+#     plt.ylabel('Time taken (in ns)')
+#     plt.legend(["n","Bucket-sort"])
+#     plt.title("Bucket_sort")
+#     plt.show()
+arr = []
+for i in range(10):
+    arr.append(random.uniform(0,1))
+
+arr = bucket_sort(arr)
+print()
+print()
+
+for val in arr:
+    print(val )
 
